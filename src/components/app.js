80838ebce93a96721e287fb0665bffe00129ac59
Jesús González <jesus92gz@gmail.com>
2016-09-18 14:10:05 +0200

    Create main App component structure

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import CategoryList from '../containers/category_list';
+import BookList from '../containers/book_list';
+import BookDetail from '../containers/book_detail';
+
 const App = () => (
 	<div className="container">
-		{/* add components here */}
+		<header>
+			<CategoryList />
+		</header>
+
+		<section id="book-store-content">
+			<BookList />
+			<BookDetail />
+		</section>
 	</div>
 );
 
