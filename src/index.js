ec8cc8b69d872fbaf6898e30aa03f96cccbaef92
Jesús González <jesus92gz@gmail.com>
2016-09-18 14:10:29 +0200

    Update React app entry point: Add middleware to the project Use redux-thunk lib Applies middleware and use it as store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 import App from './components/app';
 
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 render(
-	<Provider store={store}>
+	<Provider store={createStoreWithMiddleware(rootReducer)}>
 		<App />
 	</Provider>, document.getElementById('app'));
