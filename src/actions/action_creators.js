1ac4f38288d2ea3f60c1057a1ff38120ff6d504d
Jesús González <jesus92gz@gmail.com>
2016-09-18 14:07:44 +0200

    Crate synchronous action creators

--- src/actions/action_creators.js
+++ src/actions/action_creators.js
@@ -1,8 +1,15 @@
-import { ACTION } from './actionTypes'
+import { ACTION, SELECT_BOOK } from './action_types';
 
 export function action(param) {
 	return {
 		type: ACTION,
 		payload: { param }
+	}
+};
+
+export function selectBook(book) {
+	return {
+		type: SELECT_BOOK,
+		payload: book
 	}
 };
